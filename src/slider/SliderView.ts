a8f56471a705b4de0b41ecd533453a98c4c3295b
MaximShmatov <externgroupmail@mail.ru>
2020-07-21 22:51:15 +0300

    Create subview Scale

--- src/slider/SliderView.ts
+++ src/slider/SliderView.ts
@@ -1,10 +1,14 @@
 import '../../node_modules/@webcomponents/webcomponentsjs/custom-elements-es5-adapter';
 import '../../node_modules/@webcomponents/webcomponentsjs/webcomponents-bundle';
+import {ISliderModel} from "./ISliderModel";
 
 
 class SliderView extends HTMLElement {
   rail: HTMLElement = document.createElement('slider-element-rail');
   thumb: HTMLElement = document.createElement('slider-element-thumb');
+  scale: HTMLElement = document.createElement('slider-element-scale');
+
+  //attributes: ISliderModel;
 
   constructor() {
     super();
@@ -15,21 +19,27 @@ class SliderView extends HTMLElement {
     this.rail.appendChild(this.thumb);
     if (this.shadowRoot) {
       this.shadowRoot.appendChild(this.rail);
+      this.shadowRoot.appendChild(this.scale);
     }
+
     console.log(this.shadowRoot);
   }
 
   connectedCallback() {
     this.attachShadow({mode: 'open'}).innerHTML = `<style>${require('./slider.css')}</style>`;
+    this.scale.setAttribute('max', <string>this.getAttribute('max'));
+    this.scale.setAttribute('min', <string>this.getAttribute('min'));
+    console.log(this.getAttribute('max'));
+    console.log(this.getAttribute('min'));
     this.render();
   }
 
   static get observedAttributes(): string[] {
-    return ['min', 'max', 'step', 'value', 'pos', 'range', 'tooltip'];
+    return ['min', 'max', 'step', 'start-value', 'end-value', 'pos', 'range', 'tooltip'];
   }
 
   attributeChangedCallback() {
-    console.log('Set attribute');
+    //console.log('Set attribute');
   }
 }
 
@@ -41,6 +51,8 @@ class Rail extends HTMLElement {
 
   connectedCallback() {
     this.setAttribute('class', 'rail');
+    this.appendChild(document.createElement('div'));
+
     //console.log('connection rail');
   }
 
@@ -50,23 +62,21 @@ class Rail extends HTMLElement {
 }
 
 class Thumb extends HTMLElement {
-  private posXorY: boolean;
-  private posWidthOrHeight: number = 0;
+  private isVertical: boolean;
+  private length: number = 0;
   private pos: number = 0;
-  private mousemove: (evt: MouseEventInit) => void = () => {
-  };
-  private mouseup: (evt: MouseEvent) => void = () => {
-  };
+  private mousemove: (evt: MouseEventInit) => void = this.onMouseMove.bind(this);
+  private mouseup: (evt: MouseEvent) => void = this.onMouseUp.bind(this);
 
   constructor(pos: boolean) {
     super();
-    this.posXorY = pos;
+    this.isVertical = pos;
     this.onmousedown = this.onMouseDown
   }
 
   connectedCallback(): void {
     this.setAttribute('class', 'thumb');
-    //console.log('connection thumb');
+
   }
 
   adoptedCallback(): void {
@@ -75,25 +85,25 @@ class Thumb extends HTMLElement {
 
   private onMouseDown(evt: MouseEvent): void {
     evt.preventDefault();
-    let paddingRight = this.parentElement ? window.getComputedStyle(this.parentElement).getPropertyValue('padding-right').slice(0, -2): 0;
-    this.posWidthOrHeight = this.parentElement ? (this.parentElement.clientWidth - Number(paddingRight)) / 100 : 0;
-    this.pos = this.offsetLeft + (evt.clientX - this.offsetLeft) - Number(this.style.left.slice(0, -1)) * this.posWidthOrHeight;
+    this.length = this.parentElement ? this.parentElement.clientWidth / 100 : 0;
+    this.pos = this.offsetLeft + (evt.clientX - this.offsetLeft) - Number(this.style.marginLeft.slice(0, -1)) * this.length;
     this.mousemove = this.onMouseMove.bind(this);
     this.mouseup = this.onMouseUp.bind(this);
     document.addEventListener('mousemove', this.mousemove);
     document.addEventListener('mouseup', this.mouseup);
+    console.log(this.offsetWidth);
   }
 
   private onMouseMove(evt: MouseEventInit): void {
     if (evt.clientX) {
-      let indent: number = (evt.clientX - this.pos) / this.posWidthOrHeight;
-      if(indent < 0) {
+      let indent: number = (evt.clientX - this.pos) / this.length;
+      if (indent < 0) {
         indent = 0;
       }
-      if(indent > 100) {
+      if (indent > 100) {
         indent = 100;
       }
-      this.style.left = `${indent}%`;
+      this.style.marginLeft = `${indent}%`;
     }
   }
 
@@ -104,9 +114,51 @@ class Thumb extends HTMLElement {
 
 }
 
+class Scale extends HTMLElement {
+  private spansTag: [HTMLSpanElement, HTMLSpanElement, HTMLSpanElement, HTMLSpanElement, HTMLSpanElement] = [
+    document.createElement('span'),
+    document.createElement('span'),
+    document.createElement('span'),
+    document.createElement('span'),
+    document.createElement('span')
+  ];
+
+  constructor() {
+    super();
+  }
+
+  render() {
+    let min: number = Number(this.getAttribute('min'));
+    let max: number = Number(this.getAttribute('max'));
+    let valuesScale = Math.round((max - min) / 4);
+    this.spansTag[0].textContent = min.toString();
+    this.spansTag[1].textContent = Math.round(valuesScale).toString();
+    this.spansTag[2].textContent = Math.round(valuesScale * 2).toString();
+    this.spansTag[3].textContent = Math.round(valuesScale * 3).toString();
+    this.spansTag[4].textContent = Math.round(max).toString();
+    for (let i = 0; i < 5; i++) {
+      this.appendChild(this.spansTag[i]);
+    }
+  }
+
+  connectedCallback() {
+    this.setAttribute('class', 'scale');
+    this.render();
+  }
+
+  static get observedAttributes(): string[] {
+    return ['min', 'max'];
+  }
+
+  attributeChangedCallback(arg: string) {
+    this.render();
+  }
+}
+
 customElements.define("slider-element", SliderView);
 customElements.define('slider-element-rail', Rail);
 customElements.define('slider-element-thumb', Thumb);
+customElements.define('slider-element-scale', Scale);
 
 
 export {SliderView};
