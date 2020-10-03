bb8479dcca1c55260902825fc24159a3295e2cb5
MaximShmatov <externgroupmail@mail.ru>
2020-10-03 12:22:00 +0300

    Fixes bug in index.html

--- webpack.build.config.js
+++ webpack.build.config.js
@@ -22,7 +22,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, 'src/index.pug')
+      template: path.resolve(__dirname, './src/index.pug')
     }),
     new webpack.ProvidePlugin({
       $: 'jquery'
